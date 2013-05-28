7bc58bf60a2001e139fcf9877161bbcffce70b78
Stephanie Stober <smstober@gmail.com>
2013-05-28 11:02:40 -0400

    Started Transitions

---
+++ app/js/c6-screenjacked-transitions.js
@@ -0,0 +1,13 @@
+// TRANSITION FROM START TO INPUT // 
+
+// slide and rotate the screen to the left
+    
+    //select start screen
+    var startScreen = $("startScreen");
+
+    //declare how animation is triggered
+
+    //animation parameters 
+    TweenLite.to(startScreen, 2 {
+        backgroundColor: "#000"
+    });

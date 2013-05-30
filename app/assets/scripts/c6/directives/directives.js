ebc0c437e29f4e7afeaf33db1709a198fb33d54b
Joshua Minzner <josh@minzner.org>
2013-05-30 06:26:32 -0700

    Moved resizing code into a directive.

--- app/assets/scripts/c6/directives/directives.js
+++ app/assets/scripts/c6/directives/directives.js
@@ -3,6 +3,39 @@
 
 'use strict';
 angular.module('c6.dir.screenJack',[])
+.directive('c6Resize', ['$window', function($window) {
+	return function($scope, $element, $attrs) {
+		$($window).resize(function() {
+			// set variable dimensions for viewport
+			var baseW = 1280,
+				baseH = 684,
+				fontSize = 28,
+			
+				//find current dimensions of window
+				winH = $window.innerHeight,
+				winW = $window.innerWidth,
+			
+				//find scale factor
+				scaleH = winH / baseH,
+				scaleW = winW / baseW,
+				scaleFactor = Math.min(scaleH, scaleW);
+			
+			//apply new dimensions to viewport
+			$element.find(".viewport").height(baseH * scaleFactor)
+				.width(baseW * scaleFactor)
+				.css("font-size", (fontSize * scaleFactor))
+				.css("margin-top", ((baseH * scaleFactor) / -2))
+				.css("margin-left", ((baseW * scaleFactor) / -2));
+			
+			//feed screen divs window dimensions
+			$element.height(winH).width(winW);
+		});
+		
+		//Resize content immediately when page is loded
+		$($window).resize();
+	}
+}])
+
 .directive('c6Question', [function(){
     return  function(scope,iElement, iAttrs) {
                 if (iAttrs.c6Question) {

d9d5ce03ed9571493ecf4de783f9bd02c8feea31
Stephanie Stober <smstober@gmail.com>
2013-05-31 17:16:58 -0400

    Debugged Transitions

--- app/js/c6-screenjacked-transitions.js
+++ app/js/c6-screenjacked-transitions.js
@@ -10,88 +10,119 @@
         tl_endStart     = new TimelineLite({paused:true}),
         videoPlayer     = document.getElementById("player");
 
+    function inputReset() {
+      inputScreen.css({
+        "-webkit-transform": "rotate(-90deg)", 
+        "-webkit-transform-origin" : "100% 0%",
+        "display": "block",
+        "opacity": "1"
+      })
+      console.log("* Input Styles Reset *");      
+    }
+
+    function vidReset() {
+      videoPlayer.style.display==="block";
+      videoPlayer.style.opacity==="0";
+      console.log("* Vid Styles Reset *");
+    }
+
+    function endReset() {
+      endScreen.css({
+        "-webkit-transform": "rotate(0)", 
+        "-webkit-transform-origin" : "100% 0%",
+        "display": "none",
+        "opacity": "0"
+      })
+      console.log("* End Styles Reset *");      
+    }
 
     // Start-to-Input //        
-    tl_startInput.to(startScreen, 2, {transformOrigin: "0% 0%", 
-                                      rotation: "90deg", 
-                                      ease: Power3.easeIn, 
-                                      alpha: 0})
-      .from(inputScreen, 2, {display: "block",
-                             transformOrigin: "100% 0%", 
-                             rotation: "-90deg", 
-                             ease: Power3.easeOut, 
-                             alpha: 0,
-                             onComplete: console.log("Start-to-Input Loaded 1/5"), 
-                             }, "-=0.5");
+    tl_startInput.to(startScreen, 2, {
+        transformOrigin: "0% 0%", 
+        rotation: "90deg", 
+        ease: Power3.easeIn, 
+        alpha: 0})
+
+      .to(inputScreen, 2, { 
+        transformOrigin: "100% 0%", 
+        rotation: "0", 
+        ease: Power3.easeOut, 
+        alpha: 1}, "-=0.5");
 
     //transition to input screen
     $(".thumbnail1").click(function() {
+        inputReset();
         tl_startInput.play()
         tl_startInput.seek(0);
+        console.log("Transition to Input");
     });
 
     //transition to start screen
     $(".btn_close").click(function(e) {
         tl_startInput.reverse()
         e.preventDefault();
+        console.log("Transition to Start");
     });
 
     // Input-to-Vid //     
-    tl_expVid.to(inputScreen, 2, {alpha: 0})
-                 .to(transition, 2, {opacity: 1}, "-=1")
-                 .to(inputScreen, 0.5, {display: "none"})
-                 .to(videoPlayer, 2, {onComplete: console.log("Input-to-Vid Loaded 2/5"), opacity: 1}, "-=0.75")
+    tl_expVid.to(inputScreen, 2, {opacity: 0})
+             .to(transition, 2, {opacity: 1}, "-=1")
+             .to(videoPlayer, 2, {display: "block", opacity: 1}, "-=1")
+             .to(inputScreen, 0.1, {display: "none"});
 
     //transition to video
     $(".btn_start").click(function(e) {
-        tl_expVid.play();
         e.preventDefault();
+        tl_expVid.play();
         setTimeout('videoPlayer.play()', 2800);
         tl_expVid.seek(0);
+        console.log("Transition to Video (Start)");  
     });
 
     // Vid-to-End //  
-    tl_vidEnd.to(endScreen, 0.1, {display: "block"})
+    tl_vidEnd.to(endScreen, 0.1, {display: "block", opacity: "0"})
              .to(videoPlayer, 1.5, {opacity: 0})
-             .to(transition, 2, {opacity: 0})
+             .to(transition, 2, {opacity: 0}, "-=0.5")
              .to(videoPlayer, 0.1, {display: "none"})
-             .to(endScreen, 2, {opacity: 1, onComplete: console.log("Vid-to-End Loaded 3/5")}, "-=2");             
+             .to(endScreen, 2, {opacity: 1}, "-=2");             
 
 
-    // automatically transitions when the video ends
-    videoPlayer.addEventListener('ended', videoEnd, false);
+        // automatically transitions when the video ends
+        videoPlayer.addEventListener('ended', videoEnd, false);
 
-    function videoEnd() {
-      tl_vidEnd.play(); //play transition animation to end screen
-      console.log("Video Ended"); //print to console
-      setTimeout('videoPlayer.currentTime=0;', 5000); //rewinds playhead to beginning 
-      tl_vidEnd.seek(0); 
-    }
+        function videoEnd() {
+          vidReset();
+          endReset();
+          //console.log(getComputedStyle(videoPlayer));
+          tl_vidEnd.play(); 
+          console.log("Video Ended, Transition to End"); 
+          setTimeout('videoPlayer.currentTime=0;', 5000); 
+          tl_vidEnd.seek(0);
+        }
 
     // End-to-Vid //  
     tl_endVid.to(endScreen, 2, {opacity: 0})
              .to(videoPlayer, 0.1, {display: "block"})
              .to(transition, 2, {opacity: 1}, "-=2")
              .to(videoPlayer, 2, {opacity: 1}, "-=0.25")
-             .to(endScreen, 0.1, {display: "none", onComplete: console.log("Vid-to-End Loaded 4/5")});   
+             .to(endScreen, 0.1, {display: "none"});      
 
+        //transition to video 1 day
+        $(".btn_edit").click(function(e) {   
+            tl_endVid.play();
+            e.preventDefault();
+            setTimeout('videoPlayer.play()', 1800);
+            tl_endVid.seek(0);
+            console.log("Transition to Video (Edit)");
+        });
 
-     //var videoPlayer = false;         
-
-    //transition to video 1 day
-    $(".btn_edit").click(function(e) {   
-        tl_endVid.play();
-        e.preventDefault();
-        setTimeout('videoPlayer.play()', 1800);
-        tl_endVid.seek(0);
-    });
-
-    $(".btn_playAgain").click(function(e) {
-        tl_endVid.play();
-        e.preventDefault();
-        setTimeout('videoPlayer.play()', 1800);
-        tl_endVid.seek(0);
-    });
+        $(".btn_playAgain").click(function(e) {
+            tl_endVid.play();
+            e.preventDefault();
+            setTimeout('videoPlayer.play()', 1800);
+            tl_endVid.seek(0);
+            console.log("Transition to Video (Replay)");
+        });
 
 
     // End-to-Start
@@ -105,14 +136,14 @@
         transformOrigin: "0% 0%",
         rotation: "0deg",
         ease: Power3.easeOut,
-        alpha: 1,
-        onComplete: console.log("End-to-Start Loaded 5/5")
-    }, "-=0.5")
+        alpha: 1
+    }, "-=0.5");
 
 
-    //transition to start
-    $(".btn_newVid").click(function(e) {
-      tl_endStart.play();
-      e.preventDefault();
-      tl_endStart.seek(0);
-    })
+        //transition to start
+        $(".btn_newVid").click(function(e) {
+          tl_endStart.play();
+          e.preventDefault();
+          tl_endStart.seek(0);
+          console.log("Transition to New Video");
+        });

dde81335fd2e48e16df2bb9857191861604fbde1
Stephanie Stober <smstober@gmail.com>
2013-06-21 16:22:46 -0400

    All Hinted and Awesome (Rounded Bubbles installed and Animations are A+++)

--- app/assets/scripts/c6/services/services.js
+++ app/assets/scripts/c6/services/services.js
@@ -214,26 +214,26 @@ angular.module('c6.svc',[])
                         'cls'     : ['lee-${index}']
                         },
                      'notes'  : [
-                        { 'ts': 7.25,'template':'${1}', 'duration':1.25 }, // { type: 'thought', pos: 'top' } 
-                        { 'ts': 16,'template':'My dramatic entrance' },
-                        { 'ts': 18,'template':'Must look tough' },
-                        { 'ts': 20,'template':'Crazy ${2} stare', 'duration': 2 },
-                        { 'ts': 22,'template':'${2} ${3}' },
-                        { 'ts': 39,'template':'Get Out' },
-                        { 'ts': 41,'template':'Or I will crush your ${4} into ${5}', 'duration':2 },
-                        { 'ts': 50,'template':'What can I say to make them leave?', 'duration':2.5 },
-                        { 'ts': 60,'template':'${5}!!!', 'duration':2 },
-                        { 'ts': 86,'template':'Did I leave the ${6} on???', 'duration':2 },
-                        { 'ts': 95,'template':'Handcuffs of Drunken ${7}!!' },
-                        { 'ts':100,'template':'Bruce Lee > ${7}!', 'duration':2 },
-                        { 'ts':116, 'template':'I\'m ${8}!', 'duration': 2 },
-                        { 'ts':118,'template':'No, you\'re a dumb Ewok', 'duration': 2 },
-                        { 'ts':120,'template':'Shut up stupid!', 'duration':2 },
-                        { 'ts':123,'template':'Good comeback ${9}', 'duration':2 },
-                        { 'ts':125,'template':'${10}', 'duration':2 },
-                        { 'ts':127.5,'template':'That made no sense', 'duration':1.5 },
-                        { 'ts':129.5,'template':'${6}?', 'duration':1.5 },
-                        { 'ts':131.5,'template':'${2} ${3}!', 'duration':1.5 }
+                        { 'ts': 7.25,'template':'${1}', 'duration':1.25, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts': 16,'template':'My dramatic entrance', tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 18,'template':'Must look tough', tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 20,'template':'Crazy ${2} stare', 'duration': 2, tail: {type:'thought', pos: 'topLeft'} },
+                        { 'ts': 22,'template':'${2} ${3}', tail: {type:'thought', pos: 'topLeft'}},
+                        { 'ts': 39,'template':'Get Out', tail: {type:'thought', pos: 'bottomRight'} },
+                        { 'ts': 41,'template':'Or I will crush your ${4} into ${5}', 'duration':2, tail: {type:'thought', pos: 'topLeft'} },
+                        { 'ts': 50,'template':'What can I say to make them leave?', 'duration':2.5, tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 60,'template':'${5}!!!', 'duration':2, tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 86,'template':'Did I leave the ${6} on???', 'duration':2, tail: {type:'thought', pos: 'bottomRight'} },
+                        { 'ts': 95,'template':'Handcuffs of Drunken ${7}!!', tail: {type:'speech', pos: 'topRight'} },
+                        { 'ts':100,'template':'Bruce Lee > ${7}!', 'duration':2, tail: {type:'speech', pos: 'topLeft'} },
+                        { 'ts':116, 'template':'I\'m ${8}!', 'duration': 2, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts':118,'template':'No, you\'re a dumb Ewok', 'duration': 2, tail: {type:'speech', pos: 'bottomRight'} },
+                        { 'ts':120,'template':'Shut up stupid!', 'duration':2, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts':123,'template':'Good comeback ${9}', 'duration':2, tail: {type:'speech', pos: 'bottomRight'} },
+                        { 'ts':125,'template':'${10}', 'duration':2, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts':127.5,'template':'That made no sense', 'duration':1.5, tail: {type:'speech', pos: 'bottomRight'} },
+                        { 'ts':129.5,'template':'${6}?', 'duration':1.5, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts':131.5,'template':'${2} ${3}!', 'duration':1.5, tail: {type:'thought', pos: 'bottomRight'} }
                     ]
                 }
             };
@@ -268,25 +268,26 @@ angular.module('c6.svc',[])
                         },
                      'notes'  : [
 
-                        { 'ts':  3,'template':'I\'ll always remember' },
-                        { 'ts':  5,'template':'The times we milked the ${1} together', 'duration' : 2 },
-                        { 'ts': 10.5,'template':'${2}', 'duration' : 2 },
-                        { 'ts': 11,'template':'${3} ${4}', 'duration' : 2},
-                        { 'ts': 24.5,'template':'Wonder if he knows I milked the ${1} with everyone', 'duration' : 3 },
-                        { 'ts': 29,'template':'Yes, I know..', 'duration' : 3 },
-                        { 'ts': 32,'template':'but I don\'t care', 'duration' : 3 },
-                        { 'ts': 35,'template':'Because I ${5} ${6} with Gandalf' },
-                        { 'ts': 41,'template':'${2}', 'duration' : 3 },
-                        { 'ts': 49,'template':'Your ${7} smells like rotten ${8}', 'duration' : 5.5 },
-                        { 'ts': 58,'template':'I\'m ready to go to ${9}!' },
-                        { 'ts': 63,'template':'Get over here you tiny ${7}', 'duration' : 2 },
-                        { 'ts': 68,'template':'I hope they have ${6} in ${9}', 'duration' : 2 },
-                        { 'ts': 75,'template':'You sail, I\'m not doing ${7} on this trip', 'duration' : 3},
-                        { 'ts': 78,'template':'Hope this ${10} can swim', 'duration' : 2 },
-                        { 'ts': 82,'template':'${2}', 'duration' : 4 },
-                        { 'ts': 83,'template':'${2}', 'duration' : 3 },
-                        { 'ts': 84,'template':'${8} ${7}', 'duration' : 2 },
-                        { 'ts': 95,'template':'I just took a ${7} in my pants', 'duration' : 5 }                    ]
+                        { 'ts':  3,'template':'I\'ll always remember', tail: {type:'speech', pos: 'bottomRight'} },
+                        { 'ts':  5,'template':'The times we milked the ${1} together', 'duration' : 2, tail: {type:'speech', pos: 'topRight'} },
+                        { 'ts': 10.5,'template':'${2}', 'duration' : 2, tail: {type:'speech', pos: 'topLeft'} },
+                        { 'ts': 11,'template':'${3} ${4}', 'duration' : 2, tail: {type:'speech', pos: 'topRight'}},
+                        { 'ts': 24.5,'template':'Wonder if he knows I milked the ${1} with everyone', 'duration' : 3, tail: {type:'speech', pos: 'topLeft'} },
+                        { 'ts': 29,'template':'Yes, I know..', 'duration' : 3, tail: {type:'speech', pos: 'bottomLeft'} },
+                        { 'ts': 32,'template':'but I don\'t care', 'duration' : 3, tail: {type:'speech', pos: 'topLeft'} },
+                        { 'ts': 35,'template':'Because I ${5} ${6} with Gandalf', tail: {type:'speech', pos: 'topLeft'} },
+                        { 'ts': 41,'template':'${2}', 'duration' : 3, tail: {type:'speech', pos: 'topRight'} },
+                        { 'ts': 49,'template':'Your ${7} smells like rotten ${8}', 'duration' : 5.5, tail: {type:'speech', pos: 'topRight'} },
+                        { 'ts': 58,'template':'I\'m ready to go to ${9}!', tail: {type:'speech', pos: 'topRight'} },
+                        { 'ts': 63,'template':'Get over here you tiny ${7}', 'duration' : 2, tail: {type:'speech', pos: 'bottomRight'} },
+                        { 'ts': 68,'template':'I hope they have ${6} in ${9}', 'duration' : 2, tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 75,'template':'You sail, I\'m not doing ${7} on this trip', 'duration' : 3, tail: {type:'speech', pos: 'bottomRight'}},
+                        { 'ts': 78,'template':'Hope this ${10} can swim', 'duration' : 2, tail: {type:'thought', pos: 'bottomLeft'} },
+                        { 'ts': 82,'template':'${2}', 'duration' : 4, tail: {type:'thought', pos: 'topLeft'} },
+                        { 'ts': 83,'template':'${2}', 'duration' : 3, tail: {type:'thought', pos: 'topLeft'} },
+                        { 'ts': 84,'template':'${8} ${7}', 'duration' : 2, tail: {type:'thought', pos: 'topLeft'} },
+                        { 'ts': 95,'template':'I just took a ${7} in my pants', 'duration' : 5, tail: {type:'speech', pos: 'topRight'} }
+                        ]
                 }
             };
         }
@@ -316,15 +317,15 @@ angular.module('c6.svc',[])
                         'cls'        : ['notebook-${index}']
                     },
                     'notes' : [
-                       { 'ts':11,'template':'Weird, smells like ${1}!' },
-                       { 'ts':16,'template':'Damn she knows I just ${2}.', 'duration' : 3 },
-                       { 'ts':19,'template':'Rain Diversion ${3}!', 'duration' : 2 },
-                       { 'ts':27,'template':'Casual laugh, think about a mutant ${4} ${5}.', 'duration' : 3 },
-                       { 'ts':34,'template':'${4} ${5}!!', 'duration' : 2.5 },
-                       { 'ts':38,'template':'Must counter or he\'ll think I\'m boring like ${6}.' },
-                       { 'ts':43,'template':'Commence over-acting.  Channel ${7}.' },
-                       { 'ts':55,'template':'umm WTF was that? She must ${8} ${9}.', 'duration' : 2.5 },
-                       { 'ts':58,'template':'I love ${9}.', 'duration': 2 }
+                       { 'ts':11,'template':'Weird, smells like ${1}!', tail: {type:'speech', pos: 'bottomLeft'} },
+                       { 'ts':16,'template':'Damn she knows I just ${2}.', 'duration' : 3, tail: {type:'thought', pos: 'bottomRight'} },
+                       { 'ts':19,'template':'Rain Diversion ${3}!', 'duration' : 2, tail: {type:'thought', pos: 'topRight'} },
+                       { 'ts':27,'template':'Casual laugh, think about a mutant ${4} ${5}.', 'duration' : 3, tail: {type:'thought', pos: 'topRight'} },
+                       { 'ts':34,'template':'${4} ${5}!!', 'duration' : 2.5, tail: {type:'thought', pos: 'topRight'} },
+                       { 'ts':38,'template':'Must counter or he\'ll think I\'m boring like ${6}.', tail: {type:'thought', pos: 'bottomLeft'} },
+                       { 'ts':43,'template':'Commence over-acting.  Channel ${7}.', tail: {type:'thought', pos: 'topLeft'} },
+                       { 'ts':55,'template':'umm WTF was that? She must ${8} ${9}.', 'duration' : 2.5, tail: {type:'thought', pos: 'topRight'} },
+                       { 'ts':58,'template':'I love ${9}.', 'duration': 2, tail: {type:'speech', pos: 'bottomLeft'} }
                     ]
                 }
             };

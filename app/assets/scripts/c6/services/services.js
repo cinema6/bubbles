d71312a38054caf048e8640f13092ba4f269ce11
Joshua Minzner <josh@minzner.org>
2013-07-02 16:45:36 -0400

    CAN HAZ CORS? YES.

--- app/assets/scripts/c6/services/services.js
+++ app/assets/scripts/c6/services/services.js
@@ -156,7 +156,7 @@ angular.module('c6.svc',[])
 			requestBodyObject.script.push(line);
 		});
 
-		$http.post('http://localhost:9000/dub/create', requestBodyObject).then(function(response) {
+		$http.post('http://c6box.local/dub/create', requestBodyObject).then(function(response) {
 			url.resolve(response.data.output);
 		}, function(error) {
 			$log.error(error);

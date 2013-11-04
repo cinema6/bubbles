a1a66cdbaaa50b770d066c5489017113840c86bf
Joshua Minzner <josh@minzner.org>
2013-11-04 15:32:28 -0500

    upgrade c6ui

--- app/assets/lib/c6ui/c6ui.js
+++ app/assets/lib/c6ui/c6ui.js
@@ -2,8 +2,8 @@
  * Copyright © Cinema6 2013 All Rights Reserved. No part of this library
  * may be reproduced without Cinema6's express consent.
  *
- * Build Version: e0c18a9, Tue Oct 29 2013 12:33:08 GMT-0400 (EDT)
- * Build Date: Tue Oct 29 2013 14:46:45 GMT-0400 (EDT)
+ * Build Version: 356a197, Mon Nov 04 2013 14:56:55 GMT-0500 (EST)
+ * Build Date: Mon Nov 04 2013 15:30:08 GMT-0500 (EST)
  */
 
 (function(){

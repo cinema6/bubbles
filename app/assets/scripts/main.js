a351bdef309d11b059688c6427b91eba731e1aa7
Joshua Minzner <josh@minzner.org>
2013-12-09 18:15:42 -0500

    Upgrade c6ui

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -67,7 +67,7 @@ var libUrl = c6.libUrl.bind(window.c6),
         libUrl('gsap/1.11.2-0-g79f8c87/TweenMax.min.js'),
         libUrl('angular/v1.1.5-0-g9a7035e/angular.min.js'),
         libUrl('ui-router/0.2.0-0-g818b0d6/angular-ui-router.min.js'),
-        libUrl('c6ui/v1.2.9-0-g6e55613/c6uilib.min.js'),
+        libUrl('c6ui/v1.2.10-0-g6d29959/c6uilib.min.js'),
     ];
 
 function loadScriptsInOrder(scriptsList, done) {
@@ -99,7 +99,7 @@ loadScriptsInOrder(libScripts, function() {
     Modernizr.load({
         test: Modernizr.touch,
         nope: [
-            libUrl('c6ui/v1.2.9-0-g6e55613/css/c6uilib--hover.min.css'),
+            libUrl('c6ui/v1.2.10-0-g6d29959/css/c6uilib--hover.min.css'),
             __C6_APP_BASE_URL__ + '/styles/Screenjack--hover.css'
         ]
     });
